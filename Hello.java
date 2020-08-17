98b2af167db23805681caeb6e3775489e921f145
BryanHa2 <69809686+BryanHa2@users.noreply.github.com>
2020-08-17 11:58:31 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,19 @@
+/****************
+*Hello
+****************
+*Bryan Ha
+*8/17/2020
+*CMSC 255 C90
+****************/
+
+public class Hello {
+	public static void main(String[] args) {
+		
+		System.out.println("Bryan Ha");
+		System.out.println("Hello");
+		System.out.println("saying hello");
+		System.out.println("July 29, 2020");
+		System.out.println("CMSC 255 C90");
+		System.out.println();
+	} 
+}
